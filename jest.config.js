87023b9a682ea0f9fa511e08f7d5a3fe04f8f179
whitesky0109 <whitesky0109@naver.com>
2020-05-15 16:26:12 +0000

    [master][new] add jest env

--- jest.config.js
+++ jest.config.js
@@ -1,25 +1,18 @@
 module.exports = {
-    verbose: false,
     transform: {
-        "^.+\\.(ts|tsx)$": "ts-jest"
+        "^.+\\.(ts|tsx)$": "ts-jest",
+        "^.+\\.jsx?$": `<rootDir>/test/jest-preprocess.js`,
     },
-    moduleDirectories: ["node_modules", "src"],
-    roots: [
-        "<rootDir>/src",
-    ],
-    testRegex: "(\\.|/)(test|spec)\\.(jsx?|tsx?)$",
+    testRegex: "(\\.|/)(test|spec)\\.([tj]sx?)$",
     moduleNameMapper: {
-        "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "test/fileMock.ts"
+        ".+\\.(css|styl|less|sass|scss)$": `identity-obj-proxy`,
+        ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": `<rootDir>/test/fileMock.js`,
     },
-    moduleFileExtensions: [
-        "ts",
-        "tsx",
-        "js",
-        "jsx",
-        "json",
-        "node"
-    ],
-    snapshotSerializers: ["enzyme-to-json/serializer"],
-    globals: { "ts-jest": { isolatedModules: true } },
-    setupFilesAfterEnv: ["<rootDir>/src/test/index.ts"],
+    moduleFileExtensions: [ "ts", "tsx", "js", "jsx", "json", "node" ],
+    testPathIgnorePatterns: [`node_modules`, `.cache`],
+    transformIgnorePatterns: [`node_modules/(?!(gatsby)/)`],
+    globals: {
+      __PATH_PREFIX__: ``,
+    },
+    setupFiles: [`<rootDir>/test/loadershim.js`],
 };
